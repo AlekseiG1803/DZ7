1d6c177882d34b41d9579de0cc40f41fd010c17e
Aleksei Golovkov <alekseig1803@gmail.com>
2023-06-10 01:49:32 +0300

    ДЗ к уроку 7

---
+++ 50/Program.cs
@@ -0,0 +1,88 @@
+﻿// Задача 50. Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, и возвращает значение этого элемента или же указание, что такого элемента нет.
+
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 17 -> такого числа в массиве нет
+
+using System;
+using static System.Console;
+
+Clear();
+
+Write("Введите количество строк массива: ");
+int rows = int.Parse(ReadLine());
+
+Write("Введите количество столбцов массива: ");
+int columns = int.Parse(ReadLine());
+
+int[,] array = GetArray(rows, columns, 0, 10);
+
+int[,] GetArray(int m, int n, int min, int max)
+{
+    int[,] result = new int[m, n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            result[i, j] = new Random().Next(min, max + 1);
+        }
+
+    }
+    return result;
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Write($"{array[i, j]} ");
+        }
+        WriteLine();
+    }
+}
+
+PrintArray(array);
+
+Write("Введите номер элемента массива через пробел: ");
+string elements = ReadLine();
+int[] number = GetArrayFromString(elements);
+
+int[] GetArrayFromString(string stringArray)
+{
+    string[] nums = stringArray.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+    int[] res = new int[nums.Length];
+
+    for (int i = 0; i < nums.Length; i++)
+    {
+        res[i] = int.Parse(nums[i]);
+    }
+    return res;
+}
+
+
+int GetElement( int[] number, int[,] array){
+
+    int element = 0;
+
+    int i = number[0];
+    int j = number[1];
+
+    if (i < array.GetLength(0)) {
+
+        if (j < array.GetLength(1)) {
+        
+            element = array[i, j];
+        }
+        else WriteLine("Такого элемента нет");
+    }
+
+    else WriteLine("Такого элемента нет");
+
+    return element;
+}
+
+WriteLine($"Элемент равен {GetElement(number, array)}");
---
+++ 52/Program.cs
@@ -0,0 +1,76 @@
+﻿// Задача 52. Задайте двумерный массив из целых чисел.
+//  Найдите среднее арифметическое элементов в каждом столбце.
+
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.*/
+
+using System;
+using static System.Console;
+
+Clear();
+
+Write("Введите количество строк массива: ");
+int rows = int.Parse(ReadLine());
+
+Write("Введите количество столбцов массива: ");
+int columns = int.Parse(ReadLine());
+
+int[,] array = GetArray(rows, columns, 0, 10);
+
+int[,] GetArray(int m, int n, int min, int max)
+{
+    int[,] result = new int[m, n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            result[i, j] = new Random().Next(min, max + 1);
+        }
+
+    }
+    return result;
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Write($"{array[i, j]} ");
+        }
+        WriteLine();
+    }
+}
+
+PrintArray(array);
+
+double[] GetAverageColumn(int[,] array) {
+
+        double[] sum = new double[array.GetLength(1)];
+     //   int denominator = array.GetLength(0);
+        double[] result = new double[sum.GetLength(0)];
+
+
+        for (int i = 0; i < array.GetLength(0); i++){
+        
+            for (int j = 0; j < array.GetLength(1); j++) {
+            
+                sum[j] += array[i, j];  
+            }
+        }
+
+        for (int i = 0; i < sum.Length; i++) {
+        
+            result[i] = sum[i] / rows;
+
+        }
+
+        return result;
+
+}
+
+WriteLine($"[{String.Join("; ", GetAverageColumn(array))}]");
